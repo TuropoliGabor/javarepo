0682137cc003ac050b9308ec7558de83f0b77118
TuropoliGabor <turopoligabor@gmail.com>
2024-10-24 15:55:19 +0200

    feladat megoldasa: hagyomanyos if szerkezettel

--- src/javagit/Javagit.java
+++ src/javagit/Javagit.java
@@ -3,9 +3,20 @@ package javagit;
 public class Javagit {
 
     public static void main(String[] args) {
-        /* ha hideg van, akkor viszek kabátot
+        /* 10 fok alatt van hideg
+            ha hideg van, akkor viszek kabátot
             egyébként nem viszek kabátot
         */
+        final int HIDEG_HATAR = 10;
+        int fok = 7;
+        boolean hidegVan = fok < HIDEG_HATAR;
+        String eredmeny = "";
+        if(hidegVan){
+            eredmeny = "Viszek kabátot";
+        }else{
+            eredmeny = "Nem viszek kabátot";
+        }
+        System.out.println(eredmeny);
     }
     
 }
